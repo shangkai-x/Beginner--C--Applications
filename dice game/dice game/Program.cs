276ac6ca014a434b0fbfb50bc3432a33cacf29c3
shang kai <shangkaih@gmail.com>
2026-08-05 20:48:19 +0100

    Add files via upload

---
+++ dice game/dice game/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dice_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+            int enemyRandomNum;
+            int playerPoints = 0;
+            int enemyPoints = 0;
+
+            Random random = new Random();
+
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine("Press any key to roll the dice");
+
+                Console.ReadKey();
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("You rolled a " + playerRandomNum);
+
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNum = random.Next(1, 7);
+                Console.WriteLine("Enemy AI rolled a " + enemyRandomNum);
+
+                if (playerRandomNum > enemyRandomNum)
+                {
+                    playerPoints++;
+                    Console.WriteLine("Player win this round!");
+                }
+                else if (playerRandomNum < enemyRandomNum) 
+                {
+                    enemyPoints++;
+                    Console.WriteLine("Enemy win this round!");
+                }
+                else
+                {
+                    Console.WriteLine("Draw!");
+                }
+                Console.WriteLine("The score is now - player : " + playerPoints + ".Enemy : " + enemyPoints + ".");
+                Console.WriteLine();
+            }
+
+            if (playerPoints > enemyPoints)
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (playerPoints < enemyPoints)
+            {
+                Console.WriteLine( "You lose!");
+            }
+            else
+            {
+                Console.WriteLine( "Draw!");
+            }
+
+            Console.ReadLine();
+          
+
+        }
+    }
+}
