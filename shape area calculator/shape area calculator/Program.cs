4b21c90c8f15db40b5de517d606d407898f043cb
shang kai <shangkaih@gmail.com>
2026-08-05 21:52:42 +0100

    Add files via upload

---
+++ shape area calculator/shape area calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape_area_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string answer;
+            float result;
+
+
+            Console.WriteLine("What shape would you like to find the area of? Please enter 'r' for rectangle or any otherkey for circle");
+
+            answer = Console.ReadLine();
+            if(answer == "r")
+            {
+                Console.WriteLine("Please enter the hight of the rectangle.");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the width of the rectangle");
+                float width = float.Parse(Console.ReadLine());
+
+                result = height * width;
+            }
+            else
+            {
+                Console.WriteLine("Please enter the radius of the circle");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius * radius);
+            }
+            Console.WriteLine("The result is " + result);
+            Console.ReadLine();
+        }
+    }
+}
